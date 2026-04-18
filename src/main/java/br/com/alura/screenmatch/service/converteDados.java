bfd68c3e99f199271228833e6e514a3390173761
Guilherme Kruszynski <guilherme.kac10@gmail.com>
2026-04-18 15:04:42 -0300

    Initial commit: screenmatch sem web

---
+++ src/main/java/br/com/alura/screenmatch/service/converteDados.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch.service;
+
+import tools.jackson.databind.ObjectMapper;
+
+public class converteDados implements IConverteDados{
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        return objectMapper.readValue(json, classe);
+    }
+}
