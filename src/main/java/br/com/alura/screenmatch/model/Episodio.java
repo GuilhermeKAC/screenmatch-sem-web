ca33d7a0fb548bbd18f2148159f1075dde33f3c7
Guilherme Kruszynski <guilherme.kac10@gmail.com>
2026-04-18 21:59:53 -0300

    Adds Episodio model and displays top rated episodes.

---
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -0,0 +1,74 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.time.LocalDate;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numero;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numero = dadosEpisodio.numero();
+        this.avaliacao = dadosEpisodio.avaliacao().matches("-?\\d+(\\.\\d+)?")
+                ? Double.parseDouble(dadosEpisodio.avaliacao())
+                : 0.0;
+        this.dataLancamento = dadosEpisodio.dataLancamento().matches("\\d{4}-\\d{2}-\\d{2}")
+                ? LocalDate.parse(dadosEpisodio.dataLancamento())
+                : null;
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numero=" + numero +
+                ", avaliacao=" + avaliacao +
+                ", dataLancamento=" + dataLancamento;
+    }
+}
